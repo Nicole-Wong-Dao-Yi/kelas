06c44b71ad8c278a05c396e39e7d89984a20664a
Nicole-Wong-Dao-Yi <163950109+Nicole-Wong-Dao-Yi@users.noreply.github.com>
2024-04-02 09:25:40 +0800

    Update penempatan-kelas.py

--- penempatan-kelas.py
+++ penempatan-kelas.py
@@ -1,1 +1,14 @@
+#atur cara menentukan penempatan kelas
 
+nama=str (input("masukkan nama anda:"))
+markah=float(input("masukkan markah anda:"))
+
+#kategori kelas
+if markah<=40:
+    print("anda ditempatkan di kelas dedikasi")
+elif markah<=60:
+    print("anda ditempatkan di kelas cerdik")
+elif markah<=80:
+    print("anda ditempatkan di kelas bijak")
+elif markah >80:
+    print("anda ditempatkan di kelas amanah")   
